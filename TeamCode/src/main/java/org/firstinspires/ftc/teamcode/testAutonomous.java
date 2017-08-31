5ceb36cc9b974781e42f41ffabcb862664078cbf
aayushiron <aayushbhat2@gmail.com>
2017-08-31 16:56:39 -0700

    ColorSensor

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testAutonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testAutonomous.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
 /**
@@ -13,18 +14,38 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 public class testAutonomous extends LinearOpMode {
     DcMotor motorLeft;
     DcMotor motorRight;
+    ColorSensor colorSensor1;
+    int blue;
+    int red;
+    int green;
     public void runOpMode(){
         motorLeft = hardwareMap.dcMotor.get("motor1");
         motorRight = hardwareMap.dcMotor.get("motor2");
 
+        Move(2000);
+
+        colorSensor1 = hardwareMap.colorSensor.get("colorSensor1");
+
+        blue = colorSensor1.blue();
+        red = colorSensor1.red();
+        green = colorSensor1.green();
+
+        if (red < blue && green < blue) {
+            Move(5000);
+
+        }
+    }
+
+
+    public void Move(int EncoderSteps) {
         motorLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         motorRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
         motorLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         motorRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
 
-        motorLeft.setTargetPosition(2000);
-        motorRight.setTargetPosition(2000);
+        motorLeft.setTargetPosition(EncoderSteps);
+        motorRight.setTargetPosition(EncoderSteps);
 
         motorLeft.setPower(1.0);
         motorRight.setPower(1.0);
